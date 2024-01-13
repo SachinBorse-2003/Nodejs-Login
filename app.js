1137da8233eeb0979223a451f48209f6fbf6913b
Sachin <Sachinkailasborse@gmail.com>
2024-01-13 10:15:20 +0530

    elastic ip

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const PORT = process.env.PORT || 3000;
 
 // Use HTTPS server
 const httpsOptions = {
-  key: fs.readFileSync('/etc/letsencrypt/live/13.233.158.7/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/13.233.158.7/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/13.234.107.102/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/13.234.107.102/fullchain.pem'),
 };
 
 const server = https.createServer(httpsOptions, app);
