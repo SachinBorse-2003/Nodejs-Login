267dba2d57114e7b192e6ecebd6ca447f9f33e2b
Sachin <Sachinkailasborse@gmail.com>
2024-01-18 23:13:40 +0530

    email fixed

--- models/User.js
+++ models/User.js
@@ -1,15 +1,16 @@
-
-// models/User.js
-
 const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
 
 const userSchema = new mongoose.Schema({
+  userId: { type: Number, required: true, unique: true },
   username: { type: String, required: true, unique: true },
-  email:{type:String,required:true,unique:true},
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+// Add auto-increment to the User schema
+userSchema.plugin(autoIncrement.plugin, { model: 'User', field: 'userId', startAt: 1 });
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
